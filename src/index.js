bc74fac7be918b00f682770b48b36957b9bb37b6
Brayan Frank <brayanfrank.sm@gmail.com>
2022-05-21 23:52:39 -0300

    implementando o regex e coleta dos links

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import fs from 'fs';
 async function getFile(filepath) {
 
     try {
-        const res = await fs.promises.readFile(filepath, 'utf-8')
-        return chalk.green(res);  
+        const text = await fs.promises.readFile(filepath, 'utf-8')
+        return extractLinks(text);  
     } catch (err) {
         return chalk.red(err);
     }
+}
+
+function extractLinks(text) {
+    const regex = /\[[^\]]*\]\([^\)]*\)/gm
+    const result = text.match(regex)
+    const arrResult = result.map(item => {
+        let [title, url] = item.split(']')
+        return {[title.slice(1, title.length)] : url.slice(1, url.length)}
+    })
+    return arrResult
 }
 
 console.log(await getFile('./src/assets/example.md'));
