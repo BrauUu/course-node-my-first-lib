68d7dfd7a146a17ab08bdc1cc1062ffd8b858381
Brayan Frank <brayanfrank.sm@gmail.com>
2022-06-06 12:16:08 -0300

    some improvements

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -2,11 +2,11 @@ import getLinks from '../files.js'
 
 describe('Testando a função getLinks()', () => {
 
-    it('Deve ser uma função', () => {
+    it('Deve validar se é uma função', () => {
         expect(typeof getLinks).toBe('function')
     })
 
-    it('Deve retornar um arreio de links de um arquivo markdown passado como parâmetro', async () => {
+    it('Deve retornar um arreio de links presentes em um arquivo markdown passado como parâmetro', async () => {
         expect(await getLinks('./src/tests/assets/file01.md')).toStrictEqual(
             [
                 { 'markdownum mihi cunctosque': "http://www.uterqueter.com/" },
@@ -16,7 +16,7 @@ describe('Testando a função getLinks()', () => {
         )
     })
 
-    it('Deve retornar um arreio de links de um conjunto de arquivos markdown presentes em um diretório passado como parâmetro', async () => {
+    it('Deve retornar um arreio de arreios de links presentes em um conjunto de arquivos markdown, encontrados em um diretório que é passado como parâmetro', async () => {
         expect(await getLinks('./src/tests/assets/')).toStrictEqual(
             [
                 [
@@ -33,9 +33,8 @@ describe('Testando a função getLinks()', () => {
         )
     })
 
-    it('Deve retornar "No one link identified" quando um arquivo markdown sem links é passado como parâmetro', async () => {
-        expect(await getLinks('./src/tests/assets/fileWithoutContent.md')).toEqual('No one link identified')
+    it('Deve retornar a mensagem "No one link identified" quando um arquivo markdown sem links é passado como parâmetro', async () => {
+        expect(await getLinks('./src/tests/assets/fileWithoutLinks.md')).toEqual('No one link identified')
     })
-
 
 })
